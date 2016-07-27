d032b6290c7f969fa74ce1d25bf41085e0c6a608
mihai <saftacatalinmihai@gmail.com>
2016-07-27 18:42:44 +0300

    euler59 - WIP (found the decoded one, remains to find the sum)

--- src/euler59.erl
+++ src/euler59.erl
@@ -10,4 +10,39 @@
 -author("mihai").
 
 %% API
--export([]).
+%%-export([get_cipher/0, decode/2]).
+
+-compile(export_all).
+
+read_cipher() ->
+  {ok, Io} = file:open("p059_cipher.txt", [read]),
+  {ok, Str} = file:read_line(Io),
+  Str.
+
+parse_cipher([C, $, | Rest]) ->
+  [list_to_integer([C]) | parse_cipher(Rest)];
+
+parse_cipher([C1, C2, $, | Rest]) ->
+  [list_to_integer([C1,C2]) | parse_cipher(Rest)];
+
+parse_cipher([C1, C2, $\n]) ->
+  [list_to_integer([C1,C2])].
+
+get_cipher() -> parse_cipher(read_cipher()).
+
+decode(_, []) -> [];
+decode([K,E,Y], [C|R]) -> [ C bxor K | decode([E,Y,K], R)].
+
+try_all_keys(Cipher) ->
+  Keys = [[A,B,C] || A <- lists:seq(97,122), B <- lists:seq(97,122), C <- lists:seq(97,122) ],
+  lists:map(fun(K) -> decode(K, Cipher) end, Keys).
+
+only_ascii_chars(Decoded) ->
+  lists:filter(
+    fun(L) ->
+      lists:all(
+        fun(C) -> (C >= 32) and (126 >= C) end, L
+      )
+    end,
+    Decoded
+  ).
